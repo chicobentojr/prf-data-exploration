8c30b2954b15ba19b0dc1ff916f7f007eabad20a
Francisco Bento <franciscobdsjunior@gmail.com>
2019-06-29 13:06:23 -0300

    Add reset filter option

--- src/index.js
+++ src/index.js
@@ -2,215 +2,176 @@ let map = null;
 let markers = null;
 let markersLayerGroup = null;
 let data = null;
+let dataFiltered = null;
+let dataFilteredAll = null;
 
-const filtersChart = document.getElementById('filters-chart');
+const filtersChart = document.getElementById("filters-chart");
 
-let charts = {}
+let charts = {};
 
-const getPointDetails = (point) => {
-    return Object.keys(point).reduce((a, k) => {
-        return `${a}${k}: ${point[k]}<br>`
-    }, '');
+const loadData = map => {
+  console.log("loading data");
+  Papa.parse("/src/data/datatran2018.csv", {
+    delimiter: ";",
+    header: true,
+    download: true,
+    complete: results => {
+      console.log("Data Loaded");
+      console.log(results);
+      dataLoaded(map, results.data.slice(0, 1000));
+      addFiltersElements(results.data[0]);
+    }
+  });
+};
+
+const getPointDetails = point => {
+  return Object.keys(point).reduce((a, k) => {
+    return `${a}${k}: ${point[k]}<br>`;
+  }, "");
+};
+
+const convertPointsToMarkers = points =>
+  points.map(p =>
+    L.circleMarker([p.latitude, p.longitude], {
+      color: "#3388ff"
+    }).bindPopup(getPointDetails(p))
+  );
+
+const resetFilter = (key) => {
+  charts[key].filterAll();
+
+  const newPoints = dataFiltered.dimension(d => d).top(Infinity)
+  updateData(newPoints)
 }
 
-const convertPointsToMarkers = (points) =>
-    points.map((p) =>
-        L.circleMarker([p.latitude, p.longitude], {
-            color: '#3388ff'
-        }).bindPopup(getPointDetails(p))
-    )
 
-const addFiltersElements = (keys) => {
-    console.log('keys', keys)
-    console.log(filtersChart)
+const addFiltersElements = obj => {
+  const keys = Object.keys(obj)
+  const notFiltered = ['id', 'latitude', 'longitude']
 
-    // keys.map(k => {
-    let a = ['pessoas', 'mortos']
-    keys.map((k, i) => {
-        // console.log('k', k)
-        if (k !== 'id') {
-            filtersChart.innerHTML += `
-            <div class="clearfix"></div>
+  keys.map((k, i) => {
+    if (notFiltered.indexOf(k) === -1 && !isNaN(obj[k])) {
+      filtersChart.innerHTML += `
                 <div>
                     <h4>${k}</h4>
-                    <div id='bar-chart-${k}'></div>
+                    <div id='bar-chart-${k}' class='.chart'>
+                      <a class='reset' href='javascript:resetFilter("${k}");'>Reset</a>
+                    </div>
                 </div>
-            `
-            console.log('adding chart')
-            console.log(i)
-            setTimeout(function () {
-                console.log('timing out')
-                addChartFilter(k)
-            }, i * 0)
-        }
-    })
-    dc.renderAll()
-}
+            `;
+      setTimeout(function () {
+        addChartFilter(k);
+      }, 0);
+    }
+  });
+  dc.renderAll();
+};
 
-const addChartFilter = (field) => {
+const addChartFilter = field => {
+  // console.log("addding filter");
 
-    console.log('addding filter')
-    console.log(data)
+  // var ndx = crossfilter(data);
+  // // var all = ndx.groupAll();
+  // var all = dataFiltered.groupAll()
 
-    var ndx = crossfilter(data);
-    var all = ndx.groupAll();
+  // console.log(all);
 
-    console.log(all)
+  charts[field] = dc.barChart(`#bar-chart-${field}`);
 
-    charts[field] = dc.barChart(`#bar-chart-${field}`);
+  var fluctuation = dataFiltered.dimension(function (d) {
+    return d[field];
+  });
+  var fluctuationGroup = fluctuation.group().reduceCount();
 
-    var fluctuation = ndx.dimension(function (d) {
-        return d[field];
-    });
-    var fluctuationGroup = fluctuation.group().reduceCount();
+  // console.log('fluctuation :', fluctuation);
+  // console.log('fluctuationGroup :', fluctuationGroup.top(Infinity));
+
+  const max = Math.max.apply(Math, fluctuationGroup.top(Infinity).map(x => x.key))
+  const min = Math.min.apply(Math, fluctuationGroup.top(Infinity).map(x => x.key))
+
+  charts[field] /* dc.barChart('#volume-month-chart', 'chartGroup') */
+    .width(300)
+    .height(180)
+    .margins({ top: 10, right: 50, bottom: 30, left: 40 })
+    .dimension(fluctuation)
+    .group(fluctuationGroup)
+    .elasticY(true)
+    // .filterPrinter(function (filters) {
+    //   // console.log(filters);
+    //   rd = fluctuation.filter(filters[0]);
+    //   nd = rd.top(Infinity);
+    //   updateData(nd);
+    //   return filters;
+    // })
+    .x(
+      d3
+        .scaleLinear()
+        .domain([min - 1, max + 1])
+        .rangeRound([0, 500])
+    );
+  charts[field].xAxis().tickFormat(function (v) {
+    return v;
+  });
+  charts[field].yAxis().ticks(5);
+
+  charts[field].render();
+};
 
 
-    charts[field] /* dc.barChart('#volume-month-chart', 'chartGroup') */
-        .width(300)
-        .height(180)
-        .margins({ top: 10, right: 50, bottom: 30, left: 40 })
-        .dimension(fluctuation)
-        .group(fluctuationGroup)
-        .elasticY(true)
-        .filterPrinter(function (filters) {
-            console.log(filters)
-            rd = fluctuation.filter(filters[0])
-            nd = rd.top(Infinity);
-            updateData(nd)
-            return filters;
-        })
-        .x(d3.scaleLinear()
-            .domain([0, 15])
-            .rangeRound([0, 500]))
-
-        ;
-    charts[field].xAxis().tickFormat(
-        function (v) { return v });
-    charts[field].yAxis().ticks(5);
-
-    charts[field].render()
-
-}
-
-const loadData = (map) => {
-    console.log('loading data');
-
-    Papa.parse('/src/data/datatran2018.csv', {
-        delimiter: ';',
-        header: true,
-        download: true,
-        complete: (results) => {
-            console.log('Data Loaded')
-            console.log(results)
-            dataLoaded(map, results.data.slice(0, 100))
-            addFiltersElements(Object.keys(results.data[0]))
-        }
-    })
-}
 
 const dataLoaded = (map, points) => {
-    data = points;
-    markers = convertPointsToMarkers(points)
+  data = points;
+  dataFiltered = crossfilter(data);
+  dataFilteredAll = dataFiltered.groupAll();
+  markers = convertPointsToMarkers(points);
 
-    markersLayerGroup = L.layerGroup(markers).addTo(map)
+  markersLayerGroup = L.layerGroup(markers).addTo(map);
 
-    // addHeatMap(map, data)
-    // plotFilters(map, points)
-}
+  // addHeatMap(map, data)
+  // plotFilters(map, points)
+};
 
 const addHeatMap = (map, data) => {
-    const heatPoints = data.map((r) => {
-        return [r.latitude, r.longitude]
-    })
+  const heatPoints = data.map(r => {
+    return [r.latitude, r.longitude];
+  });
 
-    L.heatLayer(heatPoints, { maxZoom: 11 }).addTo(map);
-}
+  L.heatLayer(heatPoints, { maxZoom: 14 }).addTo(map);
+};
 
 const plotMap = () => {
-    var map = L.map('map', {
-        preferCanvas: true,
-    }).setView([-30.0699828, -51.1198806], 4);
+  var map = L.map("map", {
+    preferCanvas: true,
+    zoomControl: false,
+  }).setView([-30.0699828, -51.1198806], 4);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
 
+  L.control.zoom({
+    position: 'bottomright'
+  }).addTo(map);
 
-    return map;
-}
+  return map;
+};
 
-const plotFilters = (map, data) => {
+const updateData = newPoints => {
+  // console.log(newPoints);
 
-    var ndx = crossfilter(data);
-    var all = ndx.groupAll();
+  markersLayerGroup.clearLayers();
 
-    console.log(all)
+  markers = convertPointsToMarkers(newPoints);
 
-    var fluctuationChart = dc.barChart('#bar-chart');
+  markersLayerGroup = L.layerGroup(markers).addTo(map);
 
-    var fluctuation = ndx.dimension(function (d) {
-        return d.pessoas;
-    });
-    var fluctuationGroup = fluctuation.group().reduceCount();
-
-    console.log('flut', fluctuation)
-    console.log('group', fluctuationGroup)
-    console.log('chart', fluctuationChart)
-
-    console.log('all', fluctuationGroup.all());
-
-    console.log('top', fluctuation.top(1)[0].pessoas)
-    console.log('bottom', fluctuation.bottom(1)[0].pessoas)
-
-
-    fluctuationChart /* dc.barChart('#volume-month-chart', 'chartGroup') */
-        .width(300)
-        .height(180)
-        .margins({ top: 10, right: 50, bottom: 30, left: 40 })
-        .dimension(fluctuation)
-        .group(fluctuationGroup)
-        .elasticY(true)
-        .filterPrinter(function (filters) {
-            console.log(filters)
-            // var filter = filters[0], s = '';
-            // s += numberFormat(filter[0]) + '% -> ' + numberFormat(filter[1]) + '%';
-            // return s;
-            rd = fluctuation.filter(filters[0])
-
-            nd = rd.top(Infinity);
-
-            updateData(nd)
-
-
-            return filters;
-        })
-        .x(d3.scaleLinear()
-            .domain([0, 15])
-            .rangeRound([0, 500]))
-
-        ;
-    fluctuationChart.xAxis().tickFormat(
-        function (v) { return v });
-    fluctuationChart.yAxis().ticks(5);
-
-    fluctuationChart.render()
-}
-
-const updateData = (newPoints) => {
-    console.log(newPoints)
-
-    markersLayerGroup.clearLayers();
-
-    markers = convertPointsToMarkers(newPoints)
-
-    markersLayerGroup = L.layerGroup(markers).addTo(map)
-
-    console.log(map)
-}
+  // console.log(map);
+};
 
 const main = () => {
-    map = plotMap()
-    loadData(map)
-}
+  map = plotMap();
+  loadData(map);
+};
 
-window.onload = main
+window.onload = main;
